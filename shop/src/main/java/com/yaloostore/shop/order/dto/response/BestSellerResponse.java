94d77299fee55f58c911f1faae643a1380ecb261
hwiju yeom <yeom456@gmail.com>
2023-02-23 18:23:16 +0900

    [Feat]: bestSeller를 찾아올 때 사용할 request DTO 객체 작성

---
+++ shop/src/main/java/com/yaloostore/shop/order/dto/response/BestSellerResponse.java
@@ -0,0 +1,27 @@
+package com.yaloostore.shop.order.dto.response;
+
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BestSellerResponse {
+
+
+    private String productName;
+
+    private String description;
+    private String thumbnailUrl;
+    private Long fixedPrice;
+    private String isbn;
+    private Long pageCount;
+    private Boolean isEbook;
+    private String ebookUrl;
+    private String publisherName;
+    private String authorName;
+
+}
