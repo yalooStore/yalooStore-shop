7cc05c41d916b7dc71e810965afca1363c9ea50f
hwiju yeom <yeom456@gmail.com>
2023-02-27 20:29:47 +0900

    [Feat]:입고 시간에 따른 새로 들어온 책 정보를 가져오는 메소드

--- shop/src/main/java/com/yaloostore/shop/product/repository/querydsl/QuerydslProductRepository.java
+++ shop/src/main/java/com/yaloostore/shop/product/repository/querydsl/QuerydslProductRepository.java
@@ -3,9 +3,11 @@ package com.yaloostore.shop.product.repository.querydsl;
 
 import com.yaloostore.shop.product.dto.response.ProductFindResponse;
 
+import com.yaloostore.shop.product.dto.response.ProductBookNewOneResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuerydslProductRepository {
@@ -18,6 +20,10 @@ public interface QuerydslProductRepository {
 
     Page<ProductFindResponse> queryFindAllProductsUser(Pageable pageable);
 
+    /**
+     * 새로 입고된 순서대로(출판 시기대로 x) 새로 입고된 상품을 10개 나열해주는 메소드입니다.
+     * */
+    List<ProductBookNewOneResponse> queryFindProductNewOne();
 
 
 }
