b791d701b13b882879cfdc1ce36d263c3435ed3a
hwiju yeom <yeom456@gmail.com>
2023-03-13 20:16:22 +0900

    Update JpaProductRepository.java

--- shop/src/main/java/com/yaloostore/shop/product/repository/jpa/JpaProductRepository.java
+++ shop/src/main/java/com/yaloostore/shop/product/repository/jpa/JpaProductRepository.java
@@ -3,7 +3,7 @@ package com.yaloostore.shop.product.repository.jpa;
 
 
 import com.yaloostore.shop.product.entity.Product;
-import com.yaloostore.shop.product.repository.common.CommandProductRepository;
+import com.yaloostore.shop.product.repository.common.CommonProductRepository;
 import org.springframework.data.repository.Repository;
 
 
@@ -11,7 +11,7 @@ import org.springframework.data.repository.Repository;
  * JpaRepository -> Repository 상속으로 변경 사용
  * */
 
-public interface JpaProductRepository extends Repository<Product, Long>, CommandProductRepository {
+public interface JpaProductRepository extends Repository<Product, Long>, CommonProductRepository {
 
 
 
