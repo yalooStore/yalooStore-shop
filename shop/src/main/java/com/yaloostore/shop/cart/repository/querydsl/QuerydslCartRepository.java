481972dbf8d2626eb4f449e95f54b227bfb14a44
hwiju yeom <yeom456@gmail.com>
2023-03-18 15:15:29 +0900

    [Feat]: 회원 아이디값으로 장바구니 조회하는 메소드 추가

--- shop/src/main/java/com/yaloostore/shop/cart/repository/querydsl/QuerydslCartRepository.java
+++ shop/src/main/java/com/yaloostore/shop/cart/repository/querydsl/QuerydslCartRepository.java
@@ -1,5 +1,9 @@
 package com.yaloostore.shop.cart.repository.querydsl;
 
+import com.yaloostore.shop.cart.entity.Cart;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface QuerydslCartRepository {
 
 
@@ -7,4 +11,11 @@ public interface QuerydslCartRepository {
      * 해당 카트안에 상품이 들어있는지 확인하는 메소드입니다.
      * */
     Boolean queryExistCartByMemberIdAndProductId(Long memberId, Long productId);
+
+
+    /**
+     * 해당 회원 아이디(Pk)값을 이용해서 장바구니를 조회하는 메소드입니다.
+     * */
+    Page<Cart> queryFindCartByMemberId(Long memberId, Pageable pageable);
+
 }
