a07ddb75da124f4c67e7725e54c185ddb849544e
hwiju yeom <yeom456@gmail.com>
2023-03-18 15:17:58 +0900

    [Feat]: query dsl을 사용한 장바구니 레포지토리

--- shop/src/main/java/com/yaloostore/shop/cart/repository/querydsl/QuerydslCartRepositoryImpl.java
+++ shop/src/main/java/com/yaloostore/shop/cart/repository/querydsl/QuerydslCartRepositoryImpl.java
@@ -1,9 +1,15 @@
 package com.yaloostore.shop.cart.repository.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.yaloostore.shop.cart.entity.Cart;
 import com.yaloostore.shop.cart.entity.QCart;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 /**
@@ -27,5 +33,28 @@ public class QuerydslCartRepositoryImpl implements QuerydslCartRepository{
                         .and(cart.pk.productId.eq(productId)
                         .and(cart.product.isDeleted.isFalse())))
                 .fetchFirst()!=null;
+    }
+
+    @Override
+    public Page<Cart> queryFindCartByMemberId(Long memberId, Pageable pageable) {
+
+        QCart cart =QCart.cart;
+
+        List<Cart> carts = factory.select(cart)
+                .from(cart)
+                .where(cart.pk.memberId.eq(memberId)
+                        .and(cart.product.isDeleted.isFalse()
+                        .and(cart.product.isExpose.isTrue())))
+                .offset((long) pageable.getPageSize() * pageable.getPageNumber())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = factory.select(cart.pk.productId.count())
+                .from(cart)
+                .where(cart.pk.memberId.eq(memberId))
+                .fetchOne();
+
+
+        return new PageImpl<>(carts, pageable, count);
     }
 }
