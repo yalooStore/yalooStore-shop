01a4d9331840899c44d06e15a37dfa87c73e6fe8
hwiju yeom <yeom456@gmail.com>
2023-03-18 15:55:49 +0900

    [Feat]: 해당 login Id를 사용해서 해당 회원의 장바구니 목록을 가져와 dto 객체로 보내는 서비스 로직 작성

--- shop/src/main/java/com/yaloostore/shop/cart/dto/response/CartResponseDto.java
+++ shop/src/main/java/com/yaloostore/shop/cart/dto/response/CartResponseDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
 
 @Getter
 @AllArgsConstructor
@@ -16,5 +17,20 @@ public class CartResponseDto {
     private String productName;
     private String thumbnailUrl;
     private Long rawPrice;
+    private Long discountPrice;
+    private Long discountPercent;
+
+    //book
+    private String publisherName;
+    private String authorName;
+
+
+//    /**
+//     * cart entity를 dto 객체로 변환시켜주는 메소드입니다.
+//     * */
+//    public static CartResponseDto fromEntity(){
+//        return CartResponseDto.builder().build();
+//    }
+
 
 }
--- shop/src/main/java/com/yaloostore/shop/cart/service/impl/QuerydslCartServiceImpl.java
+++ shop/src/main/java/com/yaloostore/shop/cart/service/impl/QuerydslCartServiceImpl.java
@@ -1,15 +1,20 @@
 package com.yaloostore.shop.cart.service.impl;
 
 import com.yaloostore.shop.cart.dto.response.CartResponseDto;
+import com.yaloostore.shop.cart.entity.Cart;
 import com.yaloostore.shop.cart.repository.querydsl.QuerydslCartRepository;
 import com.yaloostore.shop.cart.service.inter.QuerydslCartService;
 import com.yaloostore.shop.common.dto.PaginationResponseDto;
 import com.yaloostore.shop.member.entity.Member;
 import com.yaloostore.shop.member.service.inter.QueryMemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -29,7 +34,37 @@ public class QuerydslCartServiceImpl implements QuerydslCartService {
 
     @Override
     public PaginationResponseDto<CartResponseDto> findCartByMemberLoginId(String loginId, Pageable pageable) {
-        return null;
+
+        Member member = queryMemberService.findByLoginId(loginId);
+
+        Page<Cart> page = querydslCartRepository.queryFindCartByMemberId(member.getMemberId(), pageable);
+
+        return getResponse(page);
+    }
+
+    private PaginationResponseDto<CartResponseDto> getResponse(Page<Cart> page) {
+
+        List<CartResponseDto> list = new ArrayList<>();
+
+        for (Cart cart : page) {
+            list.add(CartResponseDto.builder()
+                            .productId(cart.getPk().getProductId())
+                            .productName(cart.getProduct().getProductName())
+                            .thumbnailUrl(cart.getProduct().getThumbnailUrl())
+                            .rawPrice(cart.getProduct().getRawPrice())
+                            .discountPrice(cart.getProduct().getDiscountPrice())
+                            .discountPercent(cart.getProduct().getDiscountPercent())
+                            .publisherName(cart.getProduct().getBook().getPublisherName())
+                            .authorName(cart.getProduct().getBook().getAuthorName())
+                    .build());
+
+        }
+        return PaginationResponseDto.<CartResponseDto>builder()
+                .currentPage(page.getNumber())
+                .totalDataCount(page.getTotalElements())
+                .totalPage(page.getTotalPages())
+                .dataList(list)
+                .build();
     }
 
 
