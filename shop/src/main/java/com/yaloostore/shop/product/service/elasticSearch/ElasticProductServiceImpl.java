f73c200d088b00c0c70badec11c378056497d54b
hwiju yeom <yeom456@gmail.com>
2023-04-07 21:42:24 +0900

    [Feat]: 사용하지 않는 메소드 삭제

--- shop/src/main/java/com/yaloostore/shop/product/service/elasticSearch/ElasticProductServiceImpl.java
+++ shop/src/main/java/com/yaloostore/shop/product/service/elasticSearch/ElasticProductServiceImpl.java
@@ -1,11 +1,10 @@
 package com.yaloostore.shop.product.service.elasticSearch;
 
-import com.yaloostore.shop.common.dto.PaginationResponseDto;
 import com.yaloostore.shop.product.dto.response.SearchProductResponseDto;
 import com.yaloostore.shop.product.dto.transfer.SearchProductTransfer;
 import com.yaloostore.shop.product.documents.SearchProduct;
-import com.yaloostore.shop.product.repository.elasticSearch.ElasticCommonProductRepository;
-import com.yaloostore.shop.product.repository.elasticSearch.ElasticProductRepository;
+import com.yaloostore.shop.product.repository.elasticSearch.common.ElasticCommonProductRepository;
+import com.yaloostore.shop.product.repository.elasticSearch.impl.SearchProductRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -13,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +22,7 @@ import java.util.stream.Collectors;
 public class ElasticProductServiceImpl implements ElasticProductService {
 
     private final ElasticCommonProductRepository elasticCommonProductRepository;
-    private final ElasticProductRepository elasticProductRepository;
+    private final SearchProductRepositoryImpl searchProductRepositoryImpl;
 
 
     /**
@@ -32,7 +30,7 @@ public class ElasticProductServiceImpl implements ElasticProductService {
      * */
     @Override
     public Page<SearchProductResponseDto> searchProductByProductName(Pageable pageable, String productName) {
-        Page<SearchProduct> searchProducts = elasticProductRepository.searchProductsByProductName(productName, pageable);
+        Page<SearchProduct> searchProducts = searchProductRepositoryImpl.searchProductsByProductName(productName, pageable);
 
 
         List<SearchProductResponseDto> response = searchProducts.stream()
@@ -42,48 +40,6 @@ public class ElasticProductServiceImpl implements ElasticProductService {
         return new PageImpl<>(response, pageable, searchProducts.getTotalElements());
     }
 
-
-    /**
-     * {@inheritDoc}
-     * */
-    @Override
-    public PaginationResponseDto<SearchProductResponseDto> searchProductByProductNamePagination(Pageable pageable, String productName) {
-
-        Page<SearchProduct> page = elasticCommonProductRepository.findByProductName(pageable, productName);
-
-
-        return getSearchProductPaginationResponse(page);
-    }
-
-    private PaginationResponseDto<SearchProductResponseDto> getSearchProductPaginationResponse(Page<SearchProduct> page) {
-
-        List<SearchProductResponseDto> list = new ArrayList<>();
-
-        for (SearchProduct searchProduct : page.getContent()) {
-            list.add(SearchProductResponseDto.builder()
-                    .productId(searchProduct.getProductId())
-                    .productName(searchProduct.getProductName())
-                    .stock(searchProduct.getStock())
-                    .productCreatedAt(searchProduct.getProductCreatedAt())
-                    .description(searchProduct.getDescription())
-                    .thumbnailUrl(searchProduct.getThumbnailUrl())
-                    .fixedPrice(searchProduct.getFixedPrice())
-                    .rawPrice(searchProduct.getRawPrice())
-                    .isSelled(searchProduct.getIsSold())
-                    .isDeleted(searchProduct.getIsDeleted())
-                    .isExpose(searchProduct.getIsExpose())
-                    .discountPercent(searchProduct.getDiscountPercent())
-                    .build());
-        }
-
-
-        return PaginationResponseDto.<SearchProductResponseDto>builder()
-                .totalDataCount(page.getTotalElements())
-                .currentPage(page.getNumber())
-                .totalPage(page.getTotalPages())
-                .dataList(list)
-                .build();
-    }
 
 
 }
