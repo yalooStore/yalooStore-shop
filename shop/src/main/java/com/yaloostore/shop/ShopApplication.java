bd1f38f8a02b0ebc2e549935c7699233bfc0a871
hwiju yeom <yeom456@gmail.com>
2023-04-18 20:19:22 +0900

    Update ShopApplication.java

--- shop/src/main/java/com/yaloostore/shop/ShopApplication.java
+++ shop/src/main/java/com/yaloostore/shop/ShopApplication.java
@@ -3,10 +3,16 @@ package com.yaloostore.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class ShopApplication {
 
     public static void main(String[] args) {
+
+
+
+
         SpringApplication.run(ShopApplication.class, args);
     }
 
