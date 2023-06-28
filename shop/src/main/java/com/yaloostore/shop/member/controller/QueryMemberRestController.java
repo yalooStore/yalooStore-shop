8f8a222d0ae28b9ec6df247eb4c8cfa1893c5e33
hwiju yeom <yeom456@gmail.com>
2023-06-28 19:39:20 +0900

    Feat: 회원 가입에 사용될 중복 체크 관련 로직 작성

--- shop/src/main/java/com/yaloostore/shop/member/controller/QueryMemberRestController.java
+++ shop/src/main/java/com/yaloostore/shop/member/controller/QueryMemberRestController.java
@@ -2,15 +2,15 @@ package com.yaloostore.shop.member.controller;
 
 
 import com.yalooStore.common_utils.dto.ResponseDto;
+import com.yaloostore.shop.member.dto.response.MemberDuplicateDto;
 import com.yaloostore.shop.member.dto.response.MemberIdResponse;
 import com.yaloostore.shop.member.service.inter.QueryMemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +37,44 @@ public class QueryMemberRestController {
                 .success(true)
                 .status(HttpStatus.OK)
                 .data(responses)
+                .build();
+
+    }
+
+    @GetMapping("/checkEmail/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto<MemberDuplicateDto> existMemberByEmail(@PathVariable String email){
+        MemberDuplicateDto response = new MemberDuplicateDto(queryMemberService.existMemberByEmail(email));
+
+        return ResponseDto.<MemberDuplicateDto>builder()
+                .status(HttpStatus.OK)
+                .success(true)
+                .data(response)
+                .build();
+
+
+    }
+    @GetMapping("/checkNickname/{nickname}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto<MemberDuplicateDto> existMemberBy(@PathVariable String nickname){
+        MemberDuplicateDto response =new MemberDuplicateDto(queryMemberService.existMemberByNickname(nickname));
+
+        return ResponseDto.<MemberDuplicateDto>builder()
+                .status(HttpStatus.OK)
+                .success(true)
+                .data(response)
+                .build();
+
+    }
+    @GetMapping("/checkPhone/{phone}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto<MemberDuplicateDto> existMemberByPhoneNumber(@PathVariable String phone){
+        MemberDuplicateDto response =new MemberDuplicateDto(queryMemberService.existMemberByPhoneNumber(phone));
+
+        return ResponseDto.<MemberDuplicateDto>builder()
+                .status(HttpStatus.OK)
+                .success(true)
+                .data(response)
                 .build();
 
     }
