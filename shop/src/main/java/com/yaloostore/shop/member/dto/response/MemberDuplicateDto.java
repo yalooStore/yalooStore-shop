902130f4f625e0a211a035a0d0eb47b97faf5048
hwiju yeom <yeom456@gmail.com>
2023-06-28 17:05:32 +0900

    Feat: 해당 회원 중복 여부를 확인하고 응답에 사용할 dto

--- shop/src/main/java/com/yaloostore/shop/member/dto/response/MemberDuplicateDto.java
+++ shop/src/main/java/com/yaloostore/shop/member/dto/response/MemberDuplicateDto.java
@@ -1,7 +1,17 @@
 package com.yaloostore.shop.member.dto.response;
 
+
+import lombok.Getter;
+
+@Getter
 public class MemberDuplicateDto {
 
+
+
     private boolean result;
+
+    public MemberDuplicateDto(boolean result){
+        this.result = result;
+    }
 
 }
