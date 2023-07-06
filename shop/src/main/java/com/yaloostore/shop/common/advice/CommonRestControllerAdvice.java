cb4e4f822fe41bf44334edb389fe6996092ed7bb
hwiju yeom <yeom456@gmail.com>
2023-07-06 13:07:46 +0900

    HttpClientErrorException을 던져주기 위해서 해당 Exception Handler 작성

--- shop/src/main/java/com/yaloostore/shop/common/advice/CommonRestControllerAdvice.java
+++ shop/src/main/java/com/yaloostore/shop/common/advice/CommonRestControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.yaloostore.shop.common.advice;
 
+import com.yalooStore.common_utils.dto.ResponseDto;
+import com.yalooStore.common_utils.exception.ClientException;
 import com.yaloostore.shop.member.exception.AlreadyExistsMember;
 import com.yaloostore.shop.member.exception.AlreadyExistsMemberProfile;
 import com.yaloostore.shop.member.exception.NotFoundMemberException;
@@ -7,11 +9,15 @@ import com.yaloostore.shop.member.exception.NotFoundMemberRoleException;
 import com.yaloostore.shop.product.exception.NotFoundProductTypeException;
 import com.yaloostore.shop.role.exception.NotFoundRoleTypeException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
@@ -28,7 +34,7 @@ public class CommonRestControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> notFoundExceptionHandler(Exception ex){
         log.error("[NOT_FOUND] notFoundExceptionHandler ", ex);
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
 
@@ -45,5 +51,17 @@ public class CommonRestControllerAdvice {
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
+
+    @ExceptionHandler(ClientException.class)
+    public ResponseEntity<ResponseDto<Object>> clientExceptionHandler(ClientException e){
+
+        ResponseDto<Object> response = ResponseDto.builder()
+                .success(false)
+                .status(e.getResponseStatus())
+                .errorMessages(List.of(e.getDisplayErrorMessage())).build();
+
+        return ResponseEntity.status(e.getResponseStatus()).body(response);
+
+    }
 
 }
